5f67639fbd3290549c76ff99313723e1dcf55dba
Ferris Chan <xxciof@163.com>
2022-07-25 19:02:18 +0800

    de commit

--- src/main/java/net/jw/git/GitTest.java
+++ src/main/java/net/jw/git/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("git hello");
         System.out.println("git hello 2");
         System.out.println("git hello 3");
+        System.out.println("git hello dev - a ");
+        System.out.println("git hello dev - b ");
     }
 }
